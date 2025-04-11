99c6445d1c3cc286a661896e28431aa3410ff5fa
shingo405nagano <makishin.nagano@gmail.com>
2025-04-11 13:08:41 +0000

    [Add] test dimensional

---
+++ tests/test_dimensional.py
@@ -0,0 +1,27 @@
+import datetime
+import sys
+print(sys.path)
+
+import numpy as np
+import pandas as pd
+import pytest
+
+from pyvalidation.utils.dimansional import dimensional_count
+
+
+@pytest.mark.parametrize(
+    "iterable, expected",
+    [
+        ([0, 1, 2], 1),
+        ([[0, 1], [2, 3]], 2),
+        ([[[0], [1]], [[2], [3]]], 3),
+        (np.array([0, 1, 2]), 1),
+        (np.array([[0, 1], [2, 3]]), 2),
+        (np.array([[[0], [1]], [[2], [3]]]), 3),
+        (pd.Series([0, 1, 2]), 1),
+        (datetime.datetime.now(), 0),
+    ]
+)
+def test_dimensional_count(iterable, expected):
+    """Test the dimensional_count function."""
+    assert dimensional_count(iterable) == expected
